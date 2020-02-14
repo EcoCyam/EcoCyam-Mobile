516ac91f63c50038a5be26fcc29a2469c719a55e
Alexandre-Serres <alexandre.serres.as@gmail.com>
2020-02-14 09:29:25 +0100

    Merge branches 'develop' and 'testAlexou' of https://github.com/EcoCyam/EcoCyam-Mobile into testAlexou
    # Conflicts:
    #	app/src/main/java/com/example/ecocyam/MainActivity.java

--- app/src/main/java/com/example/ecocyam/MainActivity.java
+++ app/src/main/java/com/example/ecocyam/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ecocyam;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.ecocyam.localDatabase.DatabaseHelper;
 
@@ -17,5 +22,34 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("test yooo");
         myDB = new DatabaseHelper(this);
 
+        openAlertDialogCreation();
+    }
+
+    public void openAlertDialogCreation() {
+        LayoutInflater li = LayoutInflater.from(this);
+        View createUser = li.inflate(R.layout.createuser, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setView(createUser);
+
+        final EditText createUserName = (EditText) createUser.findViewById(R.id.createUserName);
+        final EditText createPassword = (EditText) createUser.findViewById(R.id.createPassword);
+// créer alertDialog avec un builder et/ou avec une méthode
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("Creer utilisateur",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                myDB.createUser(createUserName.getText().toString(), createPassword.getText().toString());
+                            }
+                        })
+                .setNegativeButton("Annuler",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
