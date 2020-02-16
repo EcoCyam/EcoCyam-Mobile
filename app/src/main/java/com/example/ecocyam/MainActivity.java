ea6cb5031f5e989f18a02d9e54f7cc3679b3d457
Alexandre-Serres <alexandre.serres.as@gmail.com>
2020-02-16 18:00:12 +0100

    "First loggin form local database"

--- app/src/main/java/com/example/ecocyam/MainActivity.java
+++ app/src/main/java/com/example/ecocyam/MainActivity.java
@@ -4,25 +4,32 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ecocyam.localDatabase.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDB;
+    Button selectB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("test yooo");
+        selectB = (Button) findViewById(R.id.select);
+
         myDB = new DatabaseHelper(this);
 
         openAlertDialogCreation();
+        selectUser();
+
     }
 
     public void openAlertDialogCreation() {
@@ -36,20 +43,82 @@ public class MainActivity extends AppCompatActivity {
 // créer alertDialog avec un builder et/ou avec une méthode
         alertDialogBuilder
                 .setCancelable(false)
-                .setPositiveButton("Creer utilisateur",
+                .setNeutralButton("Create user",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                myDB.createUser(createUserName.getText().toString(), createPassword.getText().toString());
+                                if (createUserName.getText().toString().isEmpty() || createPassword.getText().toString().isEmpty()){
+                                    Toast.makeText(MainActivity.this, "No Empty fields", Toast.LENGTH_LONG).show();
+
+                                }
+                                else {
+                                    boolean testCreate = myDB.isUserExist(createUserName.getText().toString());
+                                    if (testCreate) {
+                                        boolean testInsert = myDB.createUser(createUserName.getText().toString(), createPassword.getText().toString());
+                                        if (testInsert)
+                                            Toast.makeText(MainActivity.this, "Creation successfull", Toast.LENGTH_LONG).show();
+                                        else
+                                            Toast.makeText(MainActivity.this, "Error creation", Toast.LENGTH_LONG).show();
+                                    } else
+                                        Toast.makeText(MainActivity.this, "User name already exists", Toast.LENGTH_LONG).show();
+                                }
+
                             }
                         })
-                .setNegativeButton("Annuler",
+                .setNegativeButton("Cancel",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 dialog.cancel();
                             }
-                        });
+                        })
+                .setPositiveButton("Connection",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                boolean testCo = myDB.testConnectionInfo(createUserName.getText().toString(), createPassword.getText().toString());
+                                if (testCo){
+                                    //open new activity
+                                    Toast.makeText(MainActivity.this, "Connection ready", Toast.LENGTH_LONG).show();
+
+                                }
+                                else
+                                    Toast.makeText(MainActivity.this, "Wrong Username or Password", Toast.LENGTH_LONG).show();
+
+                            }
+                        })
+        ;
 
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    public void selectUser(){
+        selectB.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDB.get_data();
+                        if (res.getCount()==0){
+                            Toast.makeText(MainActivity.this, "insert successfull", Toast.LENGTH_LONG).show();
+                            trace("error","nothing found");
+                            return;
+                        }
+                        StringBuffer buffer = new StringBuffer();
+                        while(res.moveToNext()){
+                            buffer.append("Id : ").append(res.getString(0)).append("\n");
+                            buffer.append("Username : ").append(res.getString(1)).append("\n");
+                            buffer.append("Password : ").append(res.getString(2)).append("\n\n");
+                        }
+                        trace("Data",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void trace(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
--- app/src/main/java/com/example/ecocyam/localDatabase/DatabaseHelper.java
+++ app/src/main/java/com/example/ecocyam/localDatabase/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.ecocyam.localDatabase;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "ecocyam.db";
+    public static final int VERSION = 2;
 
     public static final String TABLE_NAME = "user_table";
     public static final String COL_ID = "ID";
@@ -17,7 +19,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_PASSWORD = "PASSWORD";
 
     public DatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, VERSION);
 
     }
 
@@ -30,6 +32,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (newVersion>oldVersion)
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
     }
@@ -48,5 +51,51 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else
             return true;
 
+    }
+
+    public Cursor get_data(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME,null);
+        return cursor;
+    }
+
+    public boolean updateUser(String id,String user, String password){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_ID,id);
+        contentValues.put(COL_NAME,user);
+        contentValues.put(COL_PASSWORD,password);
+
+        db.update(TABLE_NAME,contentValues,"ID = ?",new String[] {id});
+        return true;
+    }
+
+    public int deleteUser(String id){
+        SQLiteDatabase db=this.getWritableDatabase();
+        return db.delete(TABLE_NAME,"ID = ?", new String[]{id});
+
+    }
+
+    public boolean isUserExist(String userName) {
+        SQLiteDatabase db=this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME + " WHERE NAME=?",new String[] {userName});
+        if (cursor.getCount()==0){
+            return true;
+        }
+        //user already exists
+        return false;
+    }
+
+    public boolean testConnectionInfo(String userName, String password) {
+        SQLiteDatabase db=this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME + " WHERE NAME=? AND PASSWORD=?",new String[] {userName,password});
+        if (cursor.getCount()==0){
+            return false;
+        }
+        //user is existed
+        return true;
     }
 }
