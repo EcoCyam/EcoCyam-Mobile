35664256cbdfc7602ab3c47074b3acd37a0d1031
Alexandre-Serres <alexandre.serres.as@gmail.com>
2020-02-24 23:14:16 +0100

    "splash screen + create contact local + moving gradient"

---
+++ app/src/main/java/com/example/ecocyam/activities/Create_User.java
@@ -0,0 +1,25 @@
+package com.example.ecocyam.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import com.example.ecocyam.R;
+
+public class Create_User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__user);
+
+
+        ConstraintLayout constraintLayout = findViewById(R.id.gradient_id);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
--- app/src/main/java/com/example/ecocyam/activities/MainActivity.java
+++ app/src/main/java/com/example/ecocyam/activities/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.ecocyam.activities;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.database.Cursor;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,7 @@ import com.example.ecocyam.localDatabase.DatabaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         selectUser();
 
     }
+
 
     public void openAlertDialogLoogin() {
         LayoutInflater li = LayoutInflater.from(this);
