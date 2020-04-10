bc7223ef9ac9a770d86fd7a5bf9605c564e5d14c
Alexandre-Serres <alexandre.serres.as@gmail.com>
2020-04-10 19:47:41 +0200

    v1 scan product

--- app/src/main/java/com/example/ecocyam/activities/ScannerActivity.java
+++ app/src/main/java/com/example/ecocyam/activities/ScannerActivity.java
@@ -6,12 +6,25 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.example.ecocyam.entities.Product;
+import com.example.ecocyam.interfaces.VolleyCallBack;
+import com.example.ecocyam.utility.ConnectionTo;
+import com.example.ecocyam.utility.CustomRequest;
 import com.google.zxing.Result;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.logging.Logger;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -19,6 +32,9 @@ public final class ScannerActivity extends AppCompatActivity implements ZXingSca
 
     /* default */ ZXingScannerView ScannerView;
     private static final int CAMERA_PERMISSION_CODE = 100;
+    /* default */private String URL = "https://ecocyam-web.cfapps.io/api/items/search";
+    /* default */static final Logger log = Logger.getLogger(LoginActivity.class.getName());
+    /* default */private Product scannedProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +49,14 @@ public final class ScannerActivity extends AppCompatActivity implements ZXingSca
     public void handleResult(Result result) {
 
         //MainActivity.textViewScan.setText(result.getText());
-        onBackPressed();
-        Intent intent = new Intent(getApplicationContext(), ProductFeaturesActivity.class);
-        startActivity(intent);
+        //onBackPressed();
+        searchItemByApi(result.getText(), new VolleyCallBack() {
+            @Override
+            public void onSuccess() {
+                ConnectionTo.switchActivityWithObejctExtra(ScannerActivity.this.getApplicationContext()
+                        ,ProductFeaturesActivity.class,getScannedProduct());
+            }
+        });
     }
 
     @Override
@@ -91,5 +112,57 @@ public final class ScannerActivity extends AppCompatActivity implements ZXingSca
                         .show();
             }
         }
+    }
+
+    public void searchItemByApi(String query, final VolleyCallBack callBack) {
+
+        JSONObject requestJsonObject = new JSONObject();
+        try {
+
+            requestJsonObject.put("barcode", query);
+            requestJsonObject.put("keyword", "");
+
+        } catch (JSONException e) {
+            log.fine(e.getMessage());
+        }
+
+        CustomRequest jsonObjReq = new CustomRequest(Request.Method.POST, URL, requestJsonObject, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                log.fine(response.toString() + "connection success");
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonobject = response.getJSONObject(i);
+                        int itemId = Integer.parseInt(jsonobject.getString("itemId"));
+                        String title = jsonobject.getString("name");
+                        //  double rating = Double.parseDouble(jsonobject.getString("overallScore"));
+                        Product product = new Product(itemId,title,"aRenseigner",0.0d,0);
+                        log.info(product.getTitle());
+                        ScannerActivity.this.setScannedProduct(product);
+                    } catch (JSONException e) {
+                        log.info(e.getMessage());
+                    }
+                }
+                callBack.onSuccess();
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        log.fine("Error: " + error.getMessage());
+                        ConnectionTo.switchActivity(ScannerActivity.this.getApplicationContext(),MainActivity.class);
+                        Toast.makeText(ScannerActivity.this.getApplicationContext(), "no product found, add this one", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
+        Volley.newRequestQueue(this).add(jsonObjReq);
+    }
+
+    public Product getScannedProduct(){
+        return this.scannedProduct;
+    }
+
+    public void setScannedProduct(Product product){
+        this.scannedProduct = product;
     }
 }
